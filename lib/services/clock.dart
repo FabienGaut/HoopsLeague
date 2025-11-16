d1579c08b122b99e52d043e2a74c61b1c31c8911
Fabien-code <fabiengg05@gmail.com>
2025-11-16 13:12:51 +0100

    clock mocker added

--- lib/services/clock.dart
+++ lib/services/clock.dart
@@ -1,8 +1,25 @@
-import 'package:flutter/foundation.dart';
-
+import 'package:timezone/data/latest.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
 /// A simple clock service that can be mocked for testing.
 /// By default, it returns the current time, but it can be subclassed
 /// to return a fixed time for tests.
 class Clock {
   DateTime now() => DateTime.now();
+}
+
+
+class LAClock extends Clock {
+  final tz.Location _laLocation;
+
+  LAClock() : _laLocation = _initLocation();
+
+  static tz.Location _initLocation() {
+    tz.initializeTimeZones();
+    return tz.getLocation('America/Los_Angeles');
+  }
+
+  @override
+  DateTime now() {
+    return tz.TZDateTime.now(_laLocation);
+  }
 }
