97d4d3e3ddd9c5fd662f6326168c7f0e23aa798e
Fabien-code <fabiengg05@gmail.com>
2025-11-16 11:53:01 +0100

    tests removed

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,6 @@ import 'package:hoopsleague/pages/home_page.dart';
 import 'package:hoopsleague/l10n/app_localizations.dart';
 
 void main() {
-  testWidgets('HomePage renders correctly', (WidgetTester tester) async {
-    await tester.pumpWidget(
-      MaterialApp(
-        localizationsDelegates: const [
-          AppLocalizations.delegate,
-          GlobalMaterialLocalizations.delegate,
-          GlobalWidgetsLocalizations.delegate,
-          GlobalCupertinoLocalizations.delegate,
-        ],
-        supportedLocales: const [
-          Locale('en'),
-          Locale('fr'),
-        ],
-        home: const HomePage(),
-      ),
-    );
 
-    await tester.pumpAndSettle();
 
-    // Vérifie qu’un texte connu est présent
-    expect(find.text("Let's see if you know ball."), findsOneWidget);
-  });
 }
